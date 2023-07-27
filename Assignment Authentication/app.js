dcda1c99c61b9b509f53e0ce45e0aac092fcb4ae
Abdul Ahad <121622598+AbdulAhad7692@users.noreply.github.com>
2023-07-27 17:54:17 +0500

    Add files via upload

--- Assignment Authentication/app.js
+++ Assignment Authentication/app.js
@@ -1,32 +1,96 @@
 
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-// import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
 
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDiiQ52dkLx_qH6MHi2r8QxVL9qCS4oAM8",
+    authDomain: "js-projects-37cca.firebaseapp.com",
+    projectId: "js-projects-37cca",
+    storageBucket: "js-projects-37cca.appspot.com",
+    messagingSenderId: "835545380676",
+    appId: "1:835545380676:web:115494582e8521e30a0911",
+    measurementId: "G-49PE67NCC3"
+};
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDiiQ52dkLx_qH6MHi2r8QxVL9qCS4oAM8",
-//     authDomain: "js-projects-37cca.firebaseapp.com",
-//     projectId: "js-projects-37cca",
-//     storageBucket: "js-projects-37cca.appspot.com",
-//     messagingSenderId: "835545380676",
-//     appId: "1:835545380676:web:115494582e8521e30a0911",
-//     measurementId: "G-49PE67NCC3"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app)
-
-
-var username = document.getElementById("userName")
-var password = document.getElementById("password")
-
-console.log("username---->",username)
-console.log("password---->",password)
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
 
 
 
 
+function signUp() {
+    var email = document.getElementById("email").value
+    var password = document.getElementById("password").value
+
+    createUserWithEmailAndPassword(auth, email, password)
+
+
+
+    console.log("username---->", email)
+    console.log("password---->", password)
+        .then((res) => {
+
+            const user = res.user;
+            console.log("User ", user);
+
+
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+
+        });
+
+}
+window.signUp = signUp
+window.login = login
+
+async function login() {
+    var userEmail = document.getElementById("userEmail").value
+    var userPassword = document.getElementById("userPassword").value
+
+    console.log(userEmail, ' && ', userPassword)
+    console.log(email.value, ' && ', password.value)
+
+
+    await signInWithEmailAndPassword(auth, userEmail, userPassword)
+        .then((userCredential) => {
+
+            const user = userCredential.user;
+            console.log(user);
+
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log('1---->', errorCode);
+            console.log('2---->', errorMessage);
+        });
+
+}
+
+// if ("abdulahad@gmail.com" == userEmail) {
+    //     console.log('login succesful')
+// } else {
+//     console.log('error')
+// }
+
+
+
+ // signInWithEmailAndPassword(auth, userEmail, password)
+    //     .then((userCredential) => {
+
+    //         const user = userCredential.user;
+    //         console.log(user);
+
+    //     })
+    //     .catch((error) => {
+    //         const errorCode = error.code;
+    //         const errorMessage = error.message;
+    //         console.log('1---->', errorCode);
+    //         console.log('2---->', errorMessage);
+    //     });
 
 
 
@@ -37,23 +101,9 @@ console.log("password---->",password)
 
 
 
-// let password = "12345678"
-
-// //CREATE
-
-// createUserWithEmailAndPassword(auth, "abdulahad@gmail.com", password)
-//     .then((res) => {
-
-//         const user = res.user;
-//         console.log("User ", user);
 
 
-//     })
-//     .catch((error) => {
-//         const errorCode = error.code;
-//         const errorMessage = error.message;
 
-//     });
 
 // //SIN IN
 
